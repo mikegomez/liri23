2cc6847d43115ac64b11e693e64711dd13684999
popstar123 <wwu3@saic.edu>
2018-10-28 14:24:33 -0500

    Changed

--- liri.js
+++ liri.js
@@ -14,10 +14,10 @@ var hello = "hello";
 switch (expression) {
     case 'my-tweets':
         getTweets();
-        break; 
+        break;
     case `spotify-this-song`:
         mySong();
-        break; 
+        break;
     case `movie-this`:
         getMovie();
         break;
@@ -26,27 +26,29 @@ switch (expression) {
         break;
 }
 
-function getTweets() {    
+function getTweets() {
     var client = new Twitter(keys.twitter);
-    var params = {screen_name: 'roommatefinder3'};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-      if (!error) {
-        for (var i=0; i<5; i++) {
-        console.log(tweets[i].text)}
-      }
-    })}
-      
-    function mySong(song) {    
-        var spotify = new Spotify(keys.spotify);
-        if(song || 'i saw the sign') {
-
-        }
-        spotify.search({ type: 'track', query: "" }, function(err, data) {
-            if (err) {
-              return console.log('Error occurred: ' + err);
+    var params = { screen_name: 'roommatefinder3' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < 5; i++) {
+                console.log(tweets[i].text)
             }
-           
-          console.log(data); 
-          });
+        }
+    });
+}
+
+function mySong(song) {
+    var spotify = new Spotify(keys.spotify);
+    if (song || 'i saw the sign') {
 
     }
+    spotify.search({ type: 'track', query: "" }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data);
+    });
+
+}
